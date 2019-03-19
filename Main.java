d02af2723b8c8bd57cd93465f7637012f7dcdbac
Marta <42357029+kickme23@users.noreply.github.com>
2019-03-19 22:02:59 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,11 +3,13 @@ package pl.sdacademy.powtorka.zadania.klasy.lock;
 public class Main {
     public static void main(String[] args) {
 
-        Lock lock = new Lock();
-        System.out.println("Randomowe hasło: " + lock.getRandomLock());
-        System.out.println("Pierwsza cyfra podniesiona o 1: " + lock.changeA());
-        System.out.println("Druga cyfra podniesiona o 1: " + lock.changeB());
-        System.out.println("Trzecia cyfra podniesiona o 1: " + lock.changeC());
+       Lock lock = new Lock();
+        System.out.println("Aktualnie ustawiona kombinacja zamka: " + lock.getRandomLock());
+
+        System.out.println("Kombinacja zamieniona o 1 _każda cyfra: " + lock.changeA() + lock.changeB() + lock.changeC());
+
+        System.out.println("Czy zamek jest otwarty? (otwarty jeżeli true ) - " + lock.isLockOpen(555));
+
 
         }
 }
